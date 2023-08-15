8496467f233430be583019da7c85ddc1a1a2461c
MusicViking <sujithroshan72001@gmail.com>
2023-08-16 02:10:32 +0530

    update

--- server.py
+++ server.py
@@ -14,13 +14,13 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-        numbers = request.json
+    numbers = request.json
     response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-        numbers = request.json
+    numbers = request.json
     response = Result(numbers['first'] - numbers['second'])
     return jsonify(response)
 
